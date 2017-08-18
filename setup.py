5805e6e941c8537e4c3811ef4b361e56419474bd
Joshua Klein <mobiusklein@gmail.com>
2017-08-18 11:24:08 -0400

    update setup

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ from setuptools import find_packages, setup
 
 setup(
     name='psims',
-    version='0.0.2',
+    version='0.0.4',
+    description="Writers and controlled vocabulary manager for PSI-MS's mzML and mzIdentML standards",
+    maintainer='Joshua Klein',
+    maintainer_email="jaklein@bu.edu",
+    zip_safe=False,
     packages=find_packages(),
+    url="https://github.com/mobiusklein/psims",
     include_package_data=True,
     install_requires=[
         "lxml",
         "six",
-        "sqlalchemy"
+        "sqlalchemy",
+        "numpy"
     ]
 )
