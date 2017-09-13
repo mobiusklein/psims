24c5fd12c112ae2736ed4af3f1ed13b4e3e4e71b
Joshua Klein <mobiusklein@gmail.com>
2017-09-13 09:47:10 -0400

    extra default URL for PSI-MS

--- psims/controlled_vocabulary/controlled_vocabulary.py
+++ psims/controlled_vocabulary/controlled_vocabulary.py
@@ -19,6 +19,8 @@ def _use_vendored_unit_obo():
 fallback = {
     ("http://psidev.cvs.sourceforge.net/*checkout*/"
      "psidev/psi/psi-ms/mzML/controlledVocabulary/psi-ms.obo"): _use_vendored_psims_obo,
+    ("http://psidev.cvs.sourceforge.net/viewvc/*checkout*/"
+     "psidev/psi/psi-ms/mzML/controlledVocabulary/psi-ms.obo"): _use_vendored_psims_obo,
     ("http://obo.cvs.sourceforge.net/*checkout*/"
      "obo/obo/ontology/phenotype/unit.obo"): _use_vendored_unit_obo
 }
@@ -146,7 +148,7 @@ class OBOCache(object):
             if uri in fallback:
                 f = fallback[uri]()
             else:
-                raise
+                raise ValueError(uri)
         return f
 
     def resolve(self, uri):
