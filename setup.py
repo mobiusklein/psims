de5f26651ca484f30690fca09a74186e612bdd44
Joshua Klein <mobiusklein@gmail.com>
2018-03-24 14:41:50 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import find_packages, setup
 
 
+with open("psims/version.py") as version_file:
+    version = None
+    for line in version_file.readlines():
+        if "version = " in line:
+            version = line.split(" = ")[1].replace("\"", "").strip()
+            print("Version is: %r" % (version,))
+            break
+    else:
+        print("Cannot determine version")
+
 setup(
     name='psims',
-    version='0.0.14',
+    version=version,
     description="Writers and controlled vocabulary manager for PSI-MS's mzML and mzIdentML standards",
     maintainer='Joshua Klein',
     maintainer_email="jaklein@bu.edu",
