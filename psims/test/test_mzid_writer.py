c03814a584d8a8ef536f0ecf4bd15ebe4cdb2cde
Joshua Klein <mobiusklein@gmail.com>
2018-04-03 22:32:00 -0400

    updated tests

--- psims/test/test_mzid_writer.py
+++ psims/test/test_mzid_writer.py
@@ -27,23 +27,44 @@ def test_write(output_path):
         f.register("SpectraData", spectra_data['id'])
         f.register("SearchDatabase", search_database['id'])
         f.register("SpectrumIdentificationList", spectrum_identification_list["id"])
+        f.register("SpectrumIdentificationProtocol", protocol['id'])
 
-        f._sequence_collection(proteins, peptides, peptide_evidence)
+        with f.sequence_collection():
+            for prot in proteins:
+                f.write_db_sequence(**prot)
+            for pep in peptides:
+                f.write_peptide(**pep)
+            for evid in peptide_evidence:
+                f.write_peptide_evidence(**evid)
 
-        with f.analysis_protocol_collection():
-            f.spectrum_identification_protocol(**protocol)
         with f.analysis_collection():
             f.SpectrumIdentification(*analysis).write(f)
+        with f.analysis_protocol_collection():
+            f.spectrum_identification_protocol(**protocol)
         with f.data_collection():
             f.inputs(source_file, search_database, spectra_data)
             with f.analysis_data():
-                f.spectrum_identification_list(**spectrum_identification_list)
+                with f.spectrum_identification_list(id=spectrum_identification_list['id']):
+                    for result in spectrum_identification_list['identification_results']:
+                        f.write_spectrum_identification_result(**result)
 
     try:
         f.format()
+        f.close()
     except OSError:
         pass
 
     reader = mzid.read(output_path)
     n_peptide_evidence = len(peptide_evidence)
     assert n_peptide_evidence == len(list(reader.iterfind("PeptideEvidence")))
+    n_spectrum_identification_results = len(spectrum_identification_list['identification_results'])
+    reader.reset()
+    assert n_spectrum_identification_results == len(list(reader.iterfind("SpectrumIdentificationResult")))
+    reader.reset()
+    protocol = next(reader.iterfind("SpectrumIdentificationProtocol"))
+    mods = protocol['ModificationParams']['SearchModification']
+    assert len(mods) == 2
+    assert mods[0]['fixedMod']
+    assert not mods[1]['fixedMod']
+    assert "unknown modification" in mods[1]
+    reader.close()
