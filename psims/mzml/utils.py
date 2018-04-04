8ced0fba92c587475de57383a9e2acc5ebbbc200
Joshua Klein <mobiusklein@gmail.com>
2018-04-03 22:30:21 -0400

    refactor utils

--- psims/mzml/utils.py
+++ psims/mzml/utils.py
@@ -1,14 +1,1 @@
-from collections import Iterable, Mapping
-
-try:
-    basestring = basestring
-except:
-    basestring = (str, bytes)
-
-
-def ensure_iterable(obj):
-    if obj is None:
-        return tuple()
-    if not isinstance(obj, Iterable) or isinstance(obj, basestring) or isinstance(obj, Mapping):
-        return [obj]
-    return obj
+from psims.utils import ensure_iterable, basestring
