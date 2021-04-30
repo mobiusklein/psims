194dba235d825707a916fe4f4fa6ada836434e26
Joshua Klein <mobiusklein@gmail.com>
2021-04-30 19:17:38 -0400

    Add clearer message about HDF5 dependencies when missing

--- psims/mzmlb/__init__.py
+++ psims/mzmlb/__init__.py
@@ -3,4 +3,8 @@ try:
 except ImportError as err:
     class MzMLbWriter(object):
         def __init__(self, *args, **kwargs):
+            import warnings
+            warnings.warn(
+                "The mzMLb component requires h5py and hdf5plugin to function. "
+                "Please install them directly, or with `pip install psims[mzmlb]`")
             raise err
