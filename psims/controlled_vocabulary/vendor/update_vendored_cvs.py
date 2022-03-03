528a39e5d02fc10e149a8680bca6e69e3e8ccac6
Joshua Klein <mobiusklein@gmail.com>
2022-03-02 23:31:20 -0500

    Update CVs

--- psims/controlled_vocabulary/vendor/update_vendored_cvs.py
+++ psims/controlled_vocabulary/vendor/update_vendored_cvs.py
@@ -15,7 +15,7 @@ registry = {
     "unit.obo": "http://ontologies.berkeleybop.org/uo.obo",
     'pato.obo': "http://ontologies.berkeleybop.org/pato.obo",
     "unimod_tables.xml": "http://www.unimod.org/xml/unimod_tables.xml",
-    "XLMOD.obo": "https://raw.githubusercontent.com/HUPO-PSI/mzIdentML/master/cv/XLMOD.obo",
+    "XLMOD.obo": "https://raw.githubusercontent.com/HUPO-PSI/xlmod-CV/main/XLMOD.obo",
     # appears to reject automated download unless User-Agent is set and is very large
     # "bto.obo": "http://www.brenda-enzymes.info/ontology/tissue/tree/update/update_files/BrendaTissueOBO",
     "go.obo": "http://purl.obolibrary.org/obo/go.obo",
