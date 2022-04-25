b36e9b42ea80e20f2c811acc6d3fc86858e4e6ee
Joshua Klein <mobiusklein@gmail.com>
2022-04-25 08:07:25 -0400

    Update versions supported; Test on Py3.10

--- setup.py
+++ setup.py
@@ -57,11 +57,8 @@ setup(
         "Topic :: Text Processing :: Markup :: XML",
         "Intended Audience :: Science/Research",
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ]
 )
